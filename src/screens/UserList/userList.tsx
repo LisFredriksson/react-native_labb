5e2797d5d96eccbf66a02bfc400069993c1c2625
Lis Fredriksson <lisfredrikson@gmail.com>
2023-11-09 12:27:21 +0100

    icons

--- src/screens/UserList/userList.tsx
+++ src/screens/UserList/userList.tsx
@@ -1,15 +1,18 @@
-import {FlatList, SafeAreaView, Text, View} from 'react-native'
+import {FlatList, SafeAreaView, Text, View, RefreshControl} from 'react-native'
 import { ListItem } from '@rneui/base';
 import { useGetUsersQuery } from '../../stores/api/usersApi'
 
 const UserList = ({navigation}) => {
-    const  {data, isLoading} = useGetUsersQuery({});
+    const  {data, isLoading, refetch} = useGetUsersQuery({});
     return (
             <SafeAreaView>
                 {isLoading ? <Text> ...Loading </Text>: (
                     <View>
                 <Text>Användare:</Text>
-                <FlatList data={data} renderItem={({ item }) => (
+                <FlatList data={data} refreshControl={
+                <RefreshControl refreshing={isLoading} onRefresh={refetch} />
+                 }
+                  renderItem={({ item }) => (
                     <ListItem key={item.id} onPress={() => { navigation.navigate('UserInfo', { user: item }) }}>
                         <ListItem.Content>
                             <ListItem.Title> {`${item.firstName} ${item.lastName}`}</ListItem.Title>
@@ -24,6 +27,3 @@ const UserList = ({navigation}) => {
 }
 
 export default UserList
-
-
-
